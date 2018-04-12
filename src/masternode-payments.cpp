d4887a644b34a1ce2b49b80ca4c955c22a396155
adirondackcoin <38266830+adirondackcoin@users.noreply.github.com>
2018-04-12 14:12:27 -0300

    Masternode Payments Fix

--- src/masternode-payments.cpp
+++ src/masternode-payments.cpp
@@ -325,7 +325,7 @@ void CMasternodePayments::FillBlockPayee(CMutableTransaction& txNew, int64_t nFe
 
         LogPrintf("Masternode payment of %s to %s\n", FormatMoney(masternodePayment).c_str(), address2.ToString().c_str());
     } else {
-		if (!fProofOfStake)
+		if (pindexPrev->nHeight < 1700)
 			txNew.vout[0].nValue = blockValue - masternodePayment;
 	}
 }
